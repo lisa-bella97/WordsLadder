13591601d05615750ffb73466f17ed4ec72d33fe
lisa-bella97 <lisa-bella97@mail.ru>
2017-11-30 18:46:41 +0300

    Refactoring

--- tests/main.cpp
+++ tests/main.cpp
@@ -6,17 +6,16 @@ void test(const std::string& inpFile, const std::string& outpFile);
 
 int main(int argc, char* argv[])
 {
-    /*test("../tests/01_1.dat", "../tests/01_2.dat", "../tests/01.ans");
+    test("../tests/01_1.dat", "../tests/01_2.dat", "../tests/01.ans");
     test("../tests/02_1.dat", "../tests/02_2.dat", "../tests/02.ans");
     test("../tests/03_1.dat", "../tests/03_2.dat", "../tests/03.ans");
-     */
-    //test("../tests/04_1.dat", "../tests/04_2.dat", "../tests/04.ans");
-    //test("../tests/05_1.dat", "../tests/05_2.dat", "../tests/05.ans");
-    /*test("../tests/06.dat", "../tests/06.ans");
+    test("../tests/04_1.dat", "../tests/04_2.dat", "../tests/04.ans");
+    test("../tests/05_1.dat", "../tests/05_2.dat", "../tests/05.ans");
+    test("../tests/06.dat", "../tests/06.ans");
     test("../tests/07.dat", "../tests/07.ans");
-    test("../tests/08.dat", "../tests/08.ans");*/
+    test("../tests/08.dat", "../tests/08.ans");
     test("../tests/09.dat", "../tests/09.ans");
-
+    
     return 0;
 }
 
